4c09ccb965ec961d49f78b98ffdfc16e3d6d72f8
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-09 06:40:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,12 +19,25 @@ client.on("guildDelete", guild => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
-client.on('message', msg => {
-  if (msg.content === Prefix + 'Ping') {
-    msg.reply('Pong');
-  }
-});
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+if(message.author.bot) return;
 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
+    
 client.on('message', msg => {
   if (msg.content === Prefix + 'basskind') {
     msg.reply('My Daddy is Basskid. Give him a Dollar');
