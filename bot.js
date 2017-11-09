d4c6627bd734b681be435e25176dd55114f557b7
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-09 06:55:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,13 +10,13 @@ client.on('ready', () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setPresence({ game: { name: 'on ${client.guilds.size} servers', type: 0 } });
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setPresence({ game: { name: 'on ${client.guilds.size} servers', type: 0 } });
 });
 
 // ?basskind
