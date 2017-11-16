a1b8ef98bc2a66c1c0a186a9baab3c05b6ccbbfc
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-16 08:32:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', msg => {
 // ?ping
 client.on('message', msg => {
   if (msg.content === Prefix + 'ping') {
-    msg.reply(':ping_pong: Pong! ${guild.name}');
+    msg.reply(':ping_pong: Pong! Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.');
   }
 });
 
