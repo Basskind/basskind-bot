a672ac37af756c89f18568794d2a19bd3b61c304
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-17 03:38:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,24 +20,19 @@ client.on('guildMemberAdd', member => {
 // ?help
 client.on('message', msg => {
   if (msg.content === Prefix + 'help') {
-    const embed = new Discord.RichEmbed()
-    .setAuthor('CMS Bot', 'https://cdn.discordapp.com/attachments/380541150156685312/380908831095521290/bk_avatar.jpg')
-
-  .setColor(0x00AE86)
-  .setDescription('This is the description of your embed message')
-  .setFooter('© ClipzZModz', 'https://put a logo link here if you want ')
-  
-  .setTimestamp()
-  .addField('Field1',
-    'Example field1')
-  
-  .addField('Field2',
-    'Example field2')
-  
-  .addField('Field3',
-    'As far as i know you can have alot of fields so feel free to copy the code and place it under this field.');
-  }
-});
+    message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: client.user.username,
+        icon_url: message.author.avatarURL
+      },
+      fields: [{
+          name: `This is an example embed generated for ${message.author.tag}!`,
+          value: `You can read more [here](https://discord.js.org/#/)!`
+        },
+      ],
+    }
+  });
 
 // ?ping
 client.on('message', msg => {
