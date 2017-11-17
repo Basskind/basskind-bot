15326dddda1ed21fb3c2b056dcf22ebeec4cc185
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-17 04:29:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
 
 // ?help (dev)
 client.on('message', msg => {
-  if (msg.content === Prefix + 'ping') {
+  if (msg.content === Prefix + 'help') {
     message.channel.send({embed: {
   color: 3447003,
   description: "A very simple Embed!"
