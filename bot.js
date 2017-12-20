1fa1db8ce52f24442f7837f86a1dbba460d30158
Basskind <33504331+Basskind@users.noreply.github.com>
2017-12-20 07:44:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,28 +27,12 @@ client.on('message', msg => {
 // ?help
 client.on('message', msg => {
   if (msg.content === Prefix + 'help') {
-    msg.reply(':warning: under construction');
-  }
-});
+  message.channel.send({embed: {
+  color: 3447003,
+  description: "A very simple Embed!"
+}});
 
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
 
-  if (message.content === Prefix + 'join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
