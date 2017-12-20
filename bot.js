de8555461b42f220b39430135c44e9233c64d405
Basskind <33504331+Basskind@users.noreply.github.com>
2017-12-20 14:10:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,30 +60,22 @@ client.on('message', msg => {
 const embed = new Discord.RichEmbed()
   .setTitle("Ein paar Worte über mich")
   .setAuthor("Basskid#9068", "https://cdn.discordapp.com/attachments/380540227380314114/392936748067454976/bk_avatar.jpg")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
   .setColor(0x0086AE)
   .setDescription("Ehemaliger Playstation 4 „The Elder Scrolls Online“ Spieler. Dort verbrachte ich wunderschöne Jahre. Im Hinblick auf Teamfähigkeit haben Konsolenspieler (zumindest sehr sehr viele) extreme Egoistische Züge, weshalb es mich wieder zurück zu meiner alten Heimat, dem PC, lockte.")
   .setFooter("© by www.basskid.de")
   .setImage("https://cdn.discordapp.com/attachments/380540227380314114/393023484835397633/mr_kartoffelkopf.png")
   .setThumbnail("https://cdn.discordapp.com/attachments/380540227380314114/392936748067454976/bk_avatar.jpg")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
   .setTimestamp()
   .setURL("http://www.basskid.de")
   .addBlankField(true)
-  .addField("Mein Rig:",
-    "Intel i7-7700 CPU, 16 GB DDR4 RAM, GeForce GTX 1060 6GB, Logitech G9x Maus, Logitech G15 Tastatur, Roccat Kave 5.1 Headset")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
+  .addField("Mein Rig:", "Intel i7-7700 CPU, 16 GB DDR4 RAM, GeForce GTX 1060 6GB, Logitech G9x Maus, Logitech G15 Tastatur, Roccat Kave 5.1 Headset")
   .addBlankField(true)
   .addField("Homepage", "[www.basskid.de](http://www.basskid.de)", true)
+  .addField("Youtube", "[www.youtube.com/channel/UCmk0Zq-LzRkhwIZc3g74QGw](https://www.youtube.com/channel/UCmk0Zq-LzRkhwIZc3g74QGw)", true)
+  .addField("Twitter", "[www.twitter.com/Basskid_TV](https://twitter.com/Basskid_TV)", true)
   .addField("Twitch", "[www.twitch.tv/basskidtv](https://www.twitch.tv/basskidtv)", true);  
-    msg.channel.send({embed});
-      }
+   msg.channel.send({embed});
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
