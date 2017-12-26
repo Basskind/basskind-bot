4f332ead55876840570810cfb42a37b6a12998b5
Basskind <33504331+Basskind@users.noreply.github.com>
2017-12-26 04:04:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('guildMemberAdd', member => {
 
 client.on('message', msg => {
     if (msg.content === Prefix + 'ping') {
-        msg.sendMessage(message.author, ":ping_pong: Pong!");
+        msg.reply(':ping_pong: Pong!');
     }
 });
 
